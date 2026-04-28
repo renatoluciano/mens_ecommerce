bcedf7ec375828c9941b51a23cc91a924764a618
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-28 11:01:17 -0300

    Passo 11: Configurados arquivos de mídia e upload de imagens

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Inclui as rotas do nosso app de roupas na página inicial do site
     path('', include('products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
